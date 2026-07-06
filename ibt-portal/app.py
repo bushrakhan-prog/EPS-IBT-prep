7fee5f429cbc49b3e455e54ece655ac013045ef0
bushrakhan-prog <bushra.khan@e-p-s.in>
2026-07-06 14:17:58 +0530

    Update app.py

--- ibt-portal/app.py
+++ ibt-portal/app.py
@@ -7,7 +7,7 @@ import json, os
 app = Flask(__name__)
 app.secret_key = 'eps-ibt-portal-secret-key'
 app.jinja_env.filters['from_json'] = json.loads
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
