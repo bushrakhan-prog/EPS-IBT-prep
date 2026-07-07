83332000e6a533063b54424c3dcdbb03b05ed401
bushrakhan-prog <bushra.khan@e-p-s.in>
2026-07-07 12:02:24 +0530

    Update app.py

--- ibt-portal/app.py
+++ ibt-portal/app.py
@@ -8,7 +8,7 @@ compat.register()
 app = Flask(__name__)
 app.secret_key = 'eps-ibt-portal-secret-key'
 app.jinja_env.filters['from_json'] = json.loads
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db').replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
